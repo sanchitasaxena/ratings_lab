7d781ed9296db1279efab0d1e0de4b623855ea53
Hackbright Student <info@hackbrightacademy.com>
2016-08-03 18:11:45 -0700

    Added login and registration html pages and routes. TODO dynamically generate url to take user to their page post-login

--- server.py
+++ server.py
@@ -33,11 +33,14 @@ def user_list():
     users = User.query.all()
     return render_template('user_list.html', users=users)
 
-@app.route('/sign_in', methods=["GET"])
-def register_form():
+@app.route('/login', methods=["GET"])
+def show_login_form():
     """ Verifies if user is already in db, if not shows registration page."""
     # Get user input from form submission, send to db and query against it
-    email = request.args.get('email')
+    email = request.args['email']
+    password = request.args['password']
+    
+
     users = User.query.all()
 
     # If user's email exists in the db, send to homepage
@@ -54,31 +57,52 @@ def register_form():
         flash('You are not in our system, please create an account.')
         return render_template('/register_form.html')
 
-@app.route('/register_form', methods=["POST"])
+@app.route('/login', methods=[''])
+def login_session():
+    """Adds user to session. Redirects user to user page."""
+
+    return redirect('/users/%s', user)
+
+@app.route('/register_form')
+def show_register_form():
+    """Displays register form to user."""
+
+    return render_template('/register_form.html')
+
+
+@app.route('/register', methods=["POST"])
 def register_process():
     """ Allows user to register with email and password. """
+
     # get from previous page instead of making them enter new ones
-    email = request.form.get('email')
-    password = request.form.get('password')
+    email = request.form['email']
+    password = request.form['password']
+    zipcode = request.form['zipcode']
+    age = int(request.form['age'])
 
     # Create a User object to give to db
-    new_User = User(email,password)
+    new_user = User(email=email, password=password,zipcode=zipcode, age=age)
 
     # Insert a new row into the user database with those attributes
-    db.session.add(new_User)
+    db.session.add(new_user)
     db.session.commit()
+    
+    user = User.query.filter_by(email=email).first()
+    user_id = str(user.user_id)
 
     # Sign in (store email in session)
     session['email'] = email
     flash('You have created an account and are signed in.')
-    return redirect('/')
+    response = redirect('/users/%s' % user_id)
+
+    return response
 
 
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
 
     connect_to_db(app)
 
